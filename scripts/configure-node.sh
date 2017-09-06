544e8e3ae72a4ebff840622f2119b8d1820875cc
Chris Hillery <ceej@couchbase.com>
2017-09-05 17:53:08 -0700

    Improve container startup configuration
     - Log to /opt/couchbase/var/lib/couchbase/logs/container-startup.log
     - Wait for Server to become available on :8091 before starting config

--- scripts/configure-node.sh
+++ scripts/configure-node.sh
@@ -1,29 +1,48 @@
 #!/bin/sh
 
 echo "Configuring Couchbase Server.  Please wait (~60 sec)..."
-sleep 15
+
 export PATH=/opt/couchbase/bin:${PATH}
 
-# Setup index and memory quota
-curl -sS http://127.0.0.1:8091/pools/default -d memoryQuota=300 -d indexMemoryQuota=500 -d cbasMemoryQuota=1024 > /dev/null
+# Log all subsequent commands to logfile. FD 3 is now the console
+# for things we want to show up in "docker logs".
+LOGFILE=/opt/couchbase/var/lib/couchbase/logs/container-startup.log
+exec 3>&1 1>>${LOGFILE} 2>&1
 
-# Setup services
-echo "Configuring Services..."
-curl -sS http://127.0.0.1:8091/node/controller/setupServices -d services=kv%2Cn1ql%2Cindex%2Ccbas > /dev/null
+while true; do
+  status=$(curl -s -w "%{http_code}" -o /dev/null http://127.0.0.1:8091/ui/index.html)
+  if [ "x$status" = "x200" ]; then
+    break
+  fi
+  echo "Server not up yet, waiting 2 seconds..."
+  sleep 2
+done
 
-# Setup credentials
-curl -sS http://127.0.0.1:8091/settings/web -d port=8091 -d username=Administrator -d password=password > /dev/null
+echo "Setting memory quotas with curl:"
+curl -sS http://127.0.0.1:8091/pools/default -d memoryQuota=300 -d indexMemoryQuota=500 -d cbasMemoryQuota=1024
+echo
 
-# Setup Memory Optimized Indexes
-curl -sS -i -u Administrator:password -X POST http://127.0.0.1:8091/settings/indexes -d 'storageMode=memory_optimized' > /dev/null
+echo "Configuring Services with curl:"
+curl -sS http://127.0.0.1:8091/node/controller/setupServices -d services=kv%2Cn1ql%2Cindex%2Ccbas
+echo
 
-# Load beer-sample bucket
+echo "Setting up credentials with curl:"
+curl -sS http://127.0.0.1:8091/settings/web -d port=8091 -d username=Administrator -d password=password
+echo
+
+echo "Enabling memory-optimized indexes with curl:"
+curl -sS -i -u Administrator:password -X POST http://127.0.0.1:8091/settings/indexes -d 'storageMode=memory_optimized'
+echo
+
+echo "Loading beer-sample with curl:"
 curl -sS -u Administrator:password -X POST http://127.0.0.1:8091/sampleBuckets/install -d '["beer-sample"]'
+echo
 
-# Load travel-sample bucket
+echo "Loading travel-sample with curl:"
 curl -sS -u Administrator:password -X POST http://127.0.0.1:8091/sampleBuckets/install -d '["travel-sample"]'
+echo
 
-echo "Configuration completed - Couchbase Admin UI: http://localhost:8091"
+echo "Configuration completed - Couchbase Admin UI: http://localhost:8091" | tee /dev/fd/3
 
 if [ "$TYPE" = "WORKER" ]; then
   sleep 15
@@ -38,4 +57,5 @@ if [ "$TYPE" = "WORKER" ]; then
   fi;
 fi;
 
-sv stop /etc/service/config-couchbase > /dev/null
+echo "Stopping config-couchbase service"
+sv stop /etc/service/config-couchbase
