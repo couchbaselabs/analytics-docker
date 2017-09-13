ec2fae614cf473cf215d3a30f61353340f8631fd
Chris Hillery <ceej@couchbase.com>
2017-09-13 01:30:20 -0700

    MB-25914: Catch initial config errors and abort container with log

--- scripts/configure-node.sh
+++ scripts/configure-node.sh
@@ -18,28 +18,48 @@ while true; do
   sleep 2
 done
 
+curl_check() {
+  status=$(curl -sS -w "%{http_code}" -o /tmp/curl.txt $*)
+  cat /tmp/curl.txt
+  rm /tmp/curl.txt
+  if [ "$status" -lt 200 -o "$status" -ge 300 ]; then
+    echo
+    echo Previous curl command returned HTTP status $status
+    cat <<EOF 1>&3
+
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+Error during initial configuration - aborting container
+Here's the log of the configuration attempt:
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+EOF
+    cat $LOGFILE 1>&3
+    echo 1>&3
+    kill -HUP 1
+  fi
+}
+
 echo "Setting memory quotas with curl:"
-curl -sS http://127.0.0.1:8091/pools/default -d memoryQuota=300 -d indexMemoryQuota=500 -d cbasMemoryQuota=1024
+curl_check http://127.0.0.1:8091/pools/default -d memoryQuota=300 -d indexMemoryQuota=500 -d cbasMemoryQuota=1024
 echo
 
 echo "Configuring Services with curl:"
-curl -sS http://127.0.0.1:8091/node/controller/setupServices -d services=kv%2Cn1ql%2Cindex%2Ccbas
+curl_check http://127.0.0.1:8091/node/controller/setupServices -d services=kv%2Cn1ql%2Cindex%2Ccbas
 echo
 
 echo "Setting up credentials with curl:"
-curl -sS http://127.0.0.1:8091/settings/web -d port=8091 -d username=Administrator -d password=password
+curl_check http://127.0.0.1:8091/settings/web -d port=8091 -d username=Administrator -d password=password
 echo
 
 echo "Enabling memory-optimized indexes with curl:"
-curl -sS -i -u Administrator:password -X POST http://127.0.0.1:8091/settings/indexes -d 'storageMode=memory_optimized'
+curl_check -u Administrator:password -X POST http://127.0.0.1:8091/settings/indexes -d 'storageMode=memory_optimized'
 echo
 
 echo "Loading beer-sample with curl:"
-curl -sS -u Administrator:password -X POST http://127.0.0.1:8091/sampleBuckets/install -d '["beer-sample"]'
+curl_check -u Administrator:password -X POST http://127.0.0.1:8091/sampleBuckets/install -d '["beer-sample"]'
 echo
 
 echo "Loading travel-sample with curl:"
-curl -sS -u Administrator:password -X POST http://127.0.0.1:8091/sampleBuckets/install -d '["travel-sample"]'
+curl_check -u Administrator:password -X POST http://127.0.0.1:8091/sampleBuckets/install -d '["travel-sample"]'
 echo
 
 echo "Configuration completed - Couchbase Admin UI: http://localhost:8091" | tee /dev/fd/3
